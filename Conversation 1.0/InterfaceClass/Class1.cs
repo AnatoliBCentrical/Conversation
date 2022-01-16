77412e304e97111a713731e06dac41e4698767fa
AnatoliB <Anatoli.b148@gmail.com>
2022-01-16 10:45:13 +0200

    2nd commit

---
+++ Conversation 1.0/InterfaceClass/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversation_1._0.LoginBase_And_Repo
+{
+    public class InterfaceClass
+    { 
+    public interface Interface1
+    {
+        void Method1()
+            {
+                Console.WriteLine("Hello");
+            }
+    }
+        public interface Interface2
+        {
+            void Method2()
+            {
+                Console.WriteLine(" World");
+            }
+        }
+    public class Class1 : Interface1
+    {
+        public void Method1()
+        {
+        }
+    }
+    public class Class2 : Interface2
+    {
+        public void Method2()
+        { }
+    }
+    }
+}
---
+++ Conversation 1.0/InterfaceClass/Class2.cs
@@ -0,0 +1,16 @@
+﻿using Conversation_1._0.LoginBase_And_Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversation_1._0.INterfaceClass
+{
+    /*
+    public class Infraweb : interfaceClass.Interface1, interfaceClass.Interface2
+    {
+
+    }
+    */
+}
